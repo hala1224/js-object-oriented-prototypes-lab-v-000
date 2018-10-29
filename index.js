70a541a663259a34030828822c19073a2b4ce204
Natalie Halawani <hala1224@gmail.com>
2018-10-29 01:35:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ function BoardMember(name, homeState, training){
   BoardMember.prototype.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
     
-  };
-}
+
+};
